9ab791f6b75a36acff7839cb6f24407d0c5e6b53
Amdounislim <amdouni.slim@gmail.com>
2021-06-29 17:44:59 +0100

    back-end

---
+++ Back-end/models/user.js
@@ -0,0 +1,12 @@
+const mongoose=require("mongoose")
+const schema = mongoose.Schema
+
+const user = new schema({
+    name : {type : String, required:true},
+    email : {type : String, required:true},
+    phone : {type : Number},
+})
+
+module.exports = User = mongoose.model("user", user)
+// const User = mongoose.model("user", user)
+// module.exports = User
